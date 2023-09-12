70be6861a6b0090a8ad2a989c312ef487bfe75d1
John Cañero <johncanero.official@gmail.com>
2023-09-12 20:19:46 +0800

    Add Navbar Images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
     import "./app.css"
+    import Header from "./Header.svelte";
 	import Footer from "./Footer.svelte";
     import '@fontsource/poppins';
 </script>
 
 <div class='flex flex-col min-h-screen'>
-
+    <Header />
+    
     <main class='box-border flex flex-col flex-1 w-full max-w-screen-lg p-4 mx-auto'>
         <slot />
     </main>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,20 @@
+<script>
+    import svelteIcon from '$lib/images/svelteIcon.png';
+    import githubIcon from '$lib/images/githubIcon.png';
+</script>
+
+<navbar class='m-4'>
+    <div class='flex justify-between'>
+        <picture>
+            <a href="https://kit.svelte.dev/" target="_blank">
+                <img width={36} height={36} src={svelteIcon} alt="SvelteKit Icon" />
+            </a>
+        </picture>
+
+        <picture class='hidden md:block'>
+            <a href="https://github.com/sveltejs/kit" target="_blank">
+                <img width={36} height={36} src={githubIcon} alt="SvelteKit Icon" />
+            </a>
+        </picture>
+    </div>
+</navbar>
